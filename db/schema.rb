e70cd1c6a011ef9e176d94f720a1a90713733e43
youssef alradi <y.radi@hotmail.com>
2022-08-31 12:42:25 +0200

    created the booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_122704) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_102329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_bookings_on_offer_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "chatrooms", force: :cascade do |t|
     t.string "name"
@@ -28,11 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_122704) do
     t.datetime "updated_at", null: false
     t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-    
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_151323) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "offers", force: :cascade do |t|
     t.string "description"
@@ -58,12 +63,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_151323) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_name"
-    t.string "password"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "offers"
+  add_foreign_key "bookings", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
   add_foreign_key "offers", "users"
