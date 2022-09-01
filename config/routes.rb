082ab17f46fe91529772d300a321bc08d2508b6c
youssef alradi <y.radi@hotmail.com>
2022-09-01 11:35:34 +0200

    create booking model

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  put '/booking/:id', to: 'bookings#update', as: :booking
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :chatrooms, only: [:show, :index] do
    resources :messages, only: :create
   end
-  
+
   resources :offers
+
 end
