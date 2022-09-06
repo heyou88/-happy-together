49d15a4691a7b40b1dfe448ba6ef88df7e151630
you he <heyou88@hotmail.com>
2022-09-06 15:15:10 +0200

    profile update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Booking.destroy_all
 Offer.destroy_all
 User.destroy_all
 
