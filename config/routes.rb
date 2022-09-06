c518c2fded3c8b8e2f8cb7d8a48ac79314dee5e0
you he <heyou88@hotmail.com>
2022-09-07 00:53:07 +0200

    save local file

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,6 @@ Rails.application.routes.draw do
   get "confirm", to: "bookings#confirm"
   get "profiles/users/:id", to: "pages#profile", as: :profile
 
-<<<<<<< HEAD
   get "map", to: "offers#map"
-=======
-
->>>>>>> master
   resources :offers
 end
